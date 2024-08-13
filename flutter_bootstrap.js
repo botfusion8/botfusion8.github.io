8414d512354a0ae8f06051bee17ed5d0aa890b4c
Aakash mishra <aakash.primotech@gmail.com>
2024-08-13 18:00:04 +0530

    text overflow issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4111053667"
+    serviceWorkerVersion: "133118842"
   }
 });
