b0b8b701fa1e1c88d84b7378aa5d1f58c248807b
Aakash mishra <aakash.primotech@gmail.com>
2024-08-13 14:22:57 +0530

    redirection testing

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1825339910"
+    serviceWorkerVersion: "1082517625"
   }
 });
